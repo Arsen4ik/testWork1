772b1e94f11e129bfea0eb738bfe3748a0553d20
AdelF <azalaradle@gmail.com>
2020-02-27 19:58:27 +0300

    Updates

---
+++ precache-manifest.0edf79e3dd46cd9dde3c9b1e10be2a18.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cdc9721bf2e4b025dae43fc7bd94c2",
+    "url": "/Arsen4ik/testWork1/index.html"
+  },
+  {
+    "revision": "093e52a69ca7ae6f81b5",
+    "url": "/Arsen4ik/testWork1/static/css/main.63f1b556.chunk.css"
+  },
+  {
+    "revision": "11c892d5b5604596d2ab",
+    "url": "/Arsen4ik/testWork1/static/js/2.95785925.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Arsen4ik/testWork1/static/js/2.95785925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093e52a69ca7ae6f81b5",
+    "url": "/Arsen4ik/testWork1/static/js/main.622e1ef7.chunk.js"
+  },
+  {
+    "revision": "87b37cbb37cb8e6d6352",
+    "url": "/Arsen4ik/testWork1/static/js/runtime-main.be96ccbf.js"
+  },
+  {
+    "revision": "3100f91bbd9e9ca9ecd00255cac7d11c",
+    "url": "/Arsen4ik/testWork1/static/media/GothamPro.3100f91b.ttf"
+  },
+  {
+    "revision": "792e5785377b31db37697448eba44281",
+    "url": "/Arsen4ik/testWork1/static/media/GothamProLight.792e5785.ttf"
+  },
+  {
+    "revision": "863b7dcd5ec2c3923122af25ce0f7e4c",
+    "url": "/Arsen4ik/testWork1/static/media/Lato-Medium.863b7dcd.ttf"
+  },
+  {
+    "revision": "3c6cfb1aebd888a0eb4c8fba94140fa6",
+    "url": "/Arsen4ik/testWork1/static/media/Lato-Semibold.3c6cfb1a.ttf"
+  },
+  {
+    "revision": "ef85b068e00bc909631d5167a4bbf86c",
+    "url": "/Arsen4ik/testWork1/static/media/NextPage.ef85b068.png"
+  },
+  {
+    "revision": "4d1293da4bd468d53b34c452ef1ac244",
+    "url": "/Arsen4ik/testWork1/static/media/page1.4d1293da.png"
+  },
+  {
+    "revision": "ecf43e581d6cc63233036773a5d28c54",
+    "url": "/Arsen4ik/testWork1/static/media/page2.ecf43e58.png"
+  },
+  {
+    "revision": "337b47606ef30cd68b4520fe5723355d",
+    "url": "/Arsen4ik/testWork1/static/media/page3-1.337b4760.png"
+  },
+  {
+    "revision": "0c0b0d33b1ff026c86f68fc7db766df9",
+    "url": "/Arsen4ik/testWork1/static/media/page3-2.0c0b0d33.png"
+  },
+  {
+    "revision": "8de92630a00f973de60d846567e9c37b",
+    "url": "/Arsen4ik/testWork1/static/media/page3-3.8de92630.png"
+  }
+]);
